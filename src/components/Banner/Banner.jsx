cd0d6ed5469ba7ab296d520a894df0f2084c2e19
ammadzahid <ammadzahid53@gmail.com>
2025-11-01 05:22:31 +0500

    Responsiveness Set

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Banner = ({title, bgImage}) => {
   return (
     <div className= 'h-[50vh] mt-25 items-center flex justify-center bg-center bg-cover relative' style={{ backgroundImage : `url(${bgImage})` }} >
-      <h2 className='text-5xl bg-white/60  rounded-xl p-5 text-zinc-800 font-bold z-10' >{title}</h2>
+      <h2 className='md:text-5xl text-2xl  bg-white/60  rounded-xl p-5 text-zinc-800 font-bold z-10' >{title}</h2>
       <div className='bg-black/60 inset-0 absolute'></div>
     </div>
   )
--- src/components/CategoryPage/CategoryPage.jsx
+++ src/components/CategoryPage/CategoryPage.jsx
@@ -4,12 +4,12 @@ import Card from '../Card/Card'
 import Banner from '../Banner/Banner'
 
 
-const CategoryPage = ({title,bgImage,categories=[]}) => {
+const CategoryPage = ({ title, bgImage, categories = [] }) => {
 
 
-    const filterProducts = categories.includes('All')?
+    const filterProducts = categories.includes('All') ?
         Productlist :
-        Productlist.filter(product => 
+        Productlist.filter(product =>
             categories.includes(product.category)
         )
     const randerProducts = filterProducts.map(product => {
@@ -25,12 +25,14 @@ const CategoryPage = ({title,bgImage,categories=[]}) => {
 
     return (
 
-        <div>
-            <Banner title={title} bgImage = {bgImage} />
-            <div className='grid grid-cols-1 md:grid-cols-4 gap-9 w-[1400px] mx-auto my-10'>
-                {randerProducts}
+        <>
+            <Banner title={title} bgImage={bgImage} />
+            <div className="max-w-[1400px] mx-auto px-10 py-20">
+                <div className='grid grid-cols-1 md:grid-cols-4 gap-9'>
+                    {randerProducts}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
